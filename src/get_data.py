a5182c4a577ad3277d0c1222405bfa6dc197e518
Theodore Lincke <thli2739@swami.int.colorado.edu>
2022-03-01 15:47:33 -0700

    added 335 sharps

--- src/get_data.py
+++ src/get_data.py
@@ -1,7 +1,7 @@
 from flares.ARDataSet import ARDataSet
 from flares.data import get_dates
 
-hnum = 335
+hnum = 401
 root = "/srv/data/thli2739"
 
 dates = get_dates(hnum, root, sort = True)
@@ -9,6 +9,6 @@ print(len(dates))
 
 ds = ARDataSet(hnum, root, dates, verbose = True)
 
-ds.segmented.to_csv("./segmented_335.csv")
-ds.baseline.to_csv("./baseline_335.csv")
-ds.sharps.to_csv("./sharps_335.csv")
+ds.segmented.to_csv("./segmented_401.csv")
+ds.baseline.to_csv("./baseline_401.csv")
+ds.sharps.to_csv("./sharps_401.csv")
